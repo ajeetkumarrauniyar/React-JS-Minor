6e4dbe667a54ea152eb7cfa17949cd30485095ce
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2024-07-10 08:46:29 +0530

    Created: AddTask component

--- taskMaster-context-api/src/components/AddTask.jsx
+++ taskMaster-context-api/src/components/AddTask.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
 const AddTask = () => {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  const addTask = (e) => {
+    e.preventDefault();
+
+    if (!todo) return;
+
+    addTodo({ todoTitle: todo, completed: false });
+    setTodo("");
+  };
+
   return (
-    <form className="mb-6">
+    <form className="mb-6" onSubmit={addTask()}>
       <div className="flex">
         <input
           type="text"
           placeholder="Add a new task"
           className="flex-grow px-4 py-2 text-gray-700 bg-gray-200 rounded-l focus:outline-none focus:bg-white"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
         />
         <button
           type="submit"
